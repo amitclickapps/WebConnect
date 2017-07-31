6954150621bb780b8fc740de2e2b919961dc64bf
Amit <amit@clickapps.co>
2017-07-31 15:32:43 +0530

    Remove version

--- app/src/main/java/webconnect/com/webconnect/WebConnect.java
+++ app/src/main/java/webconnect/com/webconnect/WebConnect.java
@@ -54,14 +54,5 @@ public class WebConnect {
         return new Builder(context, url);
     }
 
-    /**
-     * With builder.
-     *
-     * @param context the context
-     * @param url     the url
-     * @return the builder
-     */
-    public static Builder with(@NonNull Context context, @NonNull String url, String test) {
-        return new Builder(context, url);
-    }
+
 }
